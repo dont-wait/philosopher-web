2d3b5c1bd7eda5eb88b00a85ab95363b30877894
Dont-wait <nguyentansang@gmail.com>
2024-09-22 20:39:29 +0700

    fix routing philosopher

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-76TCD5AY.mjs";import"./chunk-YY3BGJQW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-YQACNLRK.mjs";import"./chunk-YY3BGJQW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PhilosophersComponent } from './philosophers/philosophers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'philosopher', component: PhilosophersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
