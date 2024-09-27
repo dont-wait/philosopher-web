62f8356bdc1556e5fb7092959ccd6633643b0746
Dont-wait <nguyentansang@gmail.com>
2024-09-28 01:19:20 +0700

    update docs

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MPQLBIUU.mjs";import"./chunk-JF7YM5AF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-KLZUWZVB.mjs";import"./chunk-JF7YM5AF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
