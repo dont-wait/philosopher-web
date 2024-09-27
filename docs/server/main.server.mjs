42da64e4ebe3cbb3b92902b1efc91be8ce94f001
Dont-wait <nguyentansang@gmail.com>
2024-09-27 22:07:39 +0700

    update philosopher-component

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7VQ32XA2.mjs";import"./chunk-YY3BGJQW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-ZPDBX72J.mjs";import"./chunk-3PGQNSNE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
